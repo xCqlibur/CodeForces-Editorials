9e68bb2c33311fb4b7079220ad77c0c85dab18a9
xCqlibur <65504712+xCqlibur@users.noreply.github.com>
2022-05-13 17:28:53 -0700

    Create 554A.cpp

---
+++ Codeforces/554A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ar array
+ 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	string s;
+	cin >> s;
+	cout << (1+s.size())*25+1;
+}
