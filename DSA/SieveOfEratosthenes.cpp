29cadb54d30fc9cad17d06fe82ed28ca82e59f3a
xCqlibur <65504712+xCqlibur@users.noreply.github.com>
2023-06-15 09:09:53 -0700

    Update and rename Sieve Of Eratosthenes to SieveOfEratosthenes.cpp

---
+++ DSA/SieveOfEratosthenes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+const int mxN=//;
+
+bool prime[mxN];
+
+void SieveOfEratosthenes() {
+	memset(prime, 1, sizeof(prime));
+	prime[0]=0, prime[1]=0;
+	for(int i=2; i*i<=mxN; ++i) {
+		if(prime[i]) {
+			for(int j=i*i; j<=mxN; j+=i) {
+				prime[j]=0;
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	SieveOfEratosthenes();
+}
